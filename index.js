b4859003ac6aa256c8c7b86ccf48a4ab83afc15d
leventecsanyi <levente.csanyi@est.tech>
2024-08-11 18:49:17 +0200

    authentication

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-app.get('/', (req, res) => res.send({ hello: 'Hello World!' }))
-app.listen(port, () => console.log("alive!"))
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("alive!"));
